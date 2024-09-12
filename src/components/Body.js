1017ea66325af1c910e4438f10a675ca6f0be3f2
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2024-09-12 23:24:33 +0530

    episode-6 fetch api directly isng swiggy url and used cors extension

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,8 +9,18 @@ const Body= () => {
 const[listofRestaurants, setListOfRestaurants]=useState(resList);
 
 useEffect(()=>{
-    console.log("useEffect called");
+    fetchData();
 },[]);
+//calling api
+const fetchData = async () =>{
+  const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  );
+
+  //converting api data into json
+  const json= await data.json();
+
+  console.log(json);
+}
 
 
 console.log("body rendered before useeffect")
