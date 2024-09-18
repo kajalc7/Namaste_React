a04fbfc8e0e1f2e29f77ed5eece04cbe59ebc0e1
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2024-09-18 23:09:38 +0530

    episode  8 creating multiple state variable inside class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 const User = (props) => {
     const [count] = useState(0);
+    const [count2] = useState(1);
     return (
         <div className="User">
-            <h1>Count: {count} </h1>
+            <h1>Count1: {count} </h1>
+            <h1>Count2: {count2} </h1>
             <h2>Name:{props.Name}</h2>
             <h3>Location:pune</h3>
             <h4>Contact:Kajalchaudhary@gmail.com</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,17 +5,19 @@ class  UserClass extends React.Component {
         super(props);
        this.state = {
         count: 0,
-       }
+        count2: 1,
+       };
     }
     
 
     render(){
         //destructure
         const {Name,Location}=this.props
-        const {count}=this.state
+        const {count,count2}=this.state
         return (
             <div className="User">
-                <h1>Count:{count}</h1>
+                <h1>Count1:{count}</h1>
+                <h1>Count2: {count2} </h1>
                 <h2>Name:{Name}</h2>
                 <h3>Location:{Location}</h3>
                 <h4>Contact:Kajalchaudhary@gmail.com</h4>
