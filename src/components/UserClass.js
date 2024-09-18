b9d742cf64dc233924594af02ff6b5e426ea233a
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2024-09-18 22:48:42 +0530

    episode  8 destructure

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,12 +6,15 @@ class  UserClass extends React.Component {
 
         console.log(props)
     }
+    
 
     render(){
+        //destructure
+        const {Name,Location}=this.props
         return (
             <div className="User">
-                <h2>Name:{this.props.Name}</h2>
-                <h3>Location:{this.props.Location}</h3>
+                <h2>Name:{Name}</h2>
+                <h3>Location:{Location}</h3>
                 <h4>Contact:Kajalchaudhary@gmail.com</h4>
             </div>
         )
