fc3851d7fd29f644dfed88d6476d4e00f07537ac
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2024-09-18 23:05:14 +0530

    episode  8 creating state variable in class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 const User = (props) => {
+    const [count] = useState(0);
     return (
         <div className="User">
+            <h1>Count: {count} </h1>
             <h2>Name:{props.Name}</h2>
             <h3>Location:pune</h3>
             <h4>Contact:Kajalchaudhary@gmail.com</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 class  UserClass extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-
-        console.log(props)
+       this.state = {
+        count: 0,
+       }
     }
     
 
     render(){
         //destructure
         const {Name,Location}=this.props
+        const {count}=this.state
         return (
             <div className="User">
+                <h1>Count:{count}</h1>
                 <h2>Name:{Name}</h2>
                 <h3>Location:{Location}</h3>
                 <h4>Contact:Kajalchaudhary@gmail.com</h4>
