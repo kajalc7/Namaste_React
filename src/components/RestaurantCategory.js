0585fdf59ba1a5d81d7df317347f5f6d772817c9
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2024-09-25 22:40:16 +0530

    episode 11 added accordion for each category without data init just skeleton for each restaurant

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,8 +1,13 @@
 const RestaurantCategory = ({data}) =>{
     console.log(data)
-    return <div>
-            ResCategory
+    return(
+        <div>
+           <div className= "w-6/12 mx-auto my-4 bg-gray-100 shadow-lg p-4 flex justify-between rounded-md">
+            <span className= "font-bold text-lg">{data.title} ({data.itemCards.length})</span>
+            <span>👇</span>
+            </div>
         </div>
+    )
 }
 
 export default RestaurantCategory;
