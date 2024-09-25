3b7ba430fcb5e31db2c5dde44cb8796945c9fef8
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2024-09-25 23:51:10 +0530

    episode 11 added list of item under each category and its name price and image

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,10 +1,18 @@
+import Itemlist from "./Itemlist";
+
 const RestaurantCategory = ({data}) =>{
-    console.log(data)
     return(
         <div>
-           <div className= "w-6/12 mx-auto my-4 bg-gray-100 shadow-lg p-4 flex justify-between rounded-md">
-            <span className= "font-bold text-lg">{data.title} ({data.itemCards.length})</span>
-            <span>👇</span>
+            {/* header */}
+           <div className= "w-6/12 mx-auto my-4 bg-gray-100 shadow-lg p-4  rounded-md">
+           <div className="flex justify-between">
+                <span className= "font-bold text-lg">
+                    {data.title} ({data.itemCards.length})
+                </span>
+                <span>👇</span>
+           </div>
+                 {/* Accordion body */}
+                <Itemlist items={data.itemCards}/>
             </div>
         </div>
     )
