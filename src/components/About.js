778b27e08d88ca15b7647a4a14ec3fae444b7fd6
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2024-09-27 21:50:40 +0530

    episode 11 implemented useContext concept in class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,9 @@
 import User from "./User";
 import UserClass from "./UserClass";
 import { Component } from "react"; // Destructuring
+import UserContext from "../utils/UserContext";
+
+//when u import usercontext in class based component react gices u super power of consumer
 
 class About extends Component {
     constructor(props) {
@@ -19,6 +22,10 @@ class About extends Component {
                 <div className="bg-gray-200 rounded-lg shadow-lg p-6 max-w-md w-full">
                     <h2 className="text-xl font-semibold mb-4 text-center">About Me</h2>
                     <UserClass Name={"Kajal Chaudhary"} Location={"Pune"} />
+                    <h3>LoggedInUser :
+                    <UserContext.Consumer>
+                        { ({loggedInUser})=> <h1>{loggedInUser}</h1>}
+                    </UserContext.Consumer></h3>
                 </div>
             </div>
         );
