f96949a567af9f2450997a2d89793e8c178f4fc6
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2024-10-02 21:56:52 +0530

    episode 13 testcase to render body component with search button part1

---
+++ src/components/__tests__/Search.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import Body from "../Body";
+import MOCK_DATA from "../mocks/mockResListData.json";
+import { act } from "react-dom/test-utils";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom"; // Import this before other libraries
+
+// Mock fetch function
+global.fetch = jest.fn(() => {
+    return Promise.resolve({
+        json: () => {
+            return Promise.resolve(MOCK_DATA);
+        },
+    });
+});
+
+it("should render the body component with search button", async () => {
+    await act(async () => 
+        render(
+            <BrowserRouter>
+                <Body />
+            </BrowserRouter>
+        )
+    );
+
+    const searchBtn = screen.getByRole("button", { name: "Search" });
+    console.log(searchBtn);
+    expect(searchBtn).toBeInTheDocument();
+});
