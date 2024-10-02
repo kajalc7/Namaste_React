49c4eb8f43d953eccf22522f18ada9016ffe902a
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2024-10-02 18:53:52 +0530

    episode 13 testcase to check restaurant card with opened label

--- src/components/__tests__/restaurantcard.test.js
+++ src/components/__tests__/restaurantcard.test.js
@@ -2,6 +2,10 @@ import { render, screen } from "@testing-library/react"
 import RestaurantCard from "../RestaurantCard"
 import Mock_Data from "../mocks/resCardMock.json"
 import "@testing-library/jest-dom"
+import { withOpenLabel } from "../RestaurantCard"
+
+// Wrapped version of RestaurantCard with the "Opened" label
+const RestaurantCardWithOpenLabel = withOpenLabel((props) => <div>{props.children}</div>);
 
 it("Should render restaurant card component with props data",()=>{
  
@@ -11,3 +15,11 @@ it("Should render restaurant card component with props data",()=>{
 
     expect(name).toBeInTheDocument();
 })
+
+it("Should render the restaurantcard with 'Opened' label", () => {
+    render(<RestaurantCardWithOpenLabel resData={Mock_Data} />);
+  
+    // Check if the "Opened" label is rendered
+    const openLabel = screen.getByText("Opened");
+    expect(openLabel).toBeInTheDocument();
+  });
