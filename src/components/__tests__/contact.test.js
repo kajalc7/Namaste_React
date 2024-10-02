d0d7e879e2a979d85e7e442eba3e1cd3301d44f8
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2024-10-02 13:31:28 +0530

    episode 13 writing testcase to check input box and placeholder

--- src/components/__tests__/contact.test.js
+++ src/components/__tests__/contact.test.js
@@ -18,13 +18,38 @@ test("Should load contact us component",()=>{
 //testcase 2
 test("Should load button inside contact us component",()=>{
 
-    //whenever u r testing ui componnet i reat u have to render that component in JSdom first
 
     render(<ContactUs/>);
 
     // const button = screen.getByRole("button")
     const button = screen.getByText("Submit")
 
-
     expect(button).toBeInTheDocument();
 })
+
+//testcase 3
+test("Should load input name inside contact us component",()=>{
+
+
+    render(<ContactUs/>);
+
+    const inputName = screen.getByPlaceholderText("name")
+
+    expect(inputName).toBeInTheDocument();
+})
+
+//testcase 4
+test("Should load 2 input boxes inside contact us component",()=>{
+
+    render(<ContactUs/>);
+
+    // this is knowna s querying
+    const inputboxes = screen.getAllByRole("textbox")
+    
+    console.log(inputboxes);//this will return jsx element that is object or virtual dom object
+    console.log(inputboxes.length);
+
+
+    expect(inputboxes.length).toBe(2);
+})
+
