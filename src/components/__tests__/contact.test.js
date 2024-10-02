67d578e27a577aa8962ba80c515fd55441fb91d7
kajalc7 <79201873+kajalc7@users.noreply.github.com>
2024-10-02 13:07:09 +0530

    episode 13 writing testcase to check button is there in contact us page

--- src/components/__tests__/contact.test.js
+++ src/components/__tests__/contact.test.js
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react"
 import ContactUs from "../ContactUs";
 import "@testing-library/jest-dom";
 
+//testcase1
 test("Should load contact us component",()=>{
 
     //whenever u r testing ui componnet i reat u have to render that component in JSdom first
@@ -12,4 +13,16 @@ test("Should load contact us component",()=>{
     const heading = screen.getByRole("heading");//this will find all heading component
 
     expect(heading).toBeInTheDocument();
+})
+
+//testcase 2
+test("Should load button inside contact us component",()=>{
+
+    //whenever u r testing ui componnet i reat u have to render that component in JSdom first
+
+    render(<ContactUs/>);
+
+    const button = screen.getByRole("button")
+
+    expect(button).toBeInTheDocument();
 })
